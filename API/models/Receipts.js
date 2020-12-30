ffb6312b5c798e6c3a8f4fe491e9d861366224b1
Chung Tin <tinanthedrew@gmail.com>
2020-12-30 10:40:27 +0700

    Add Receipt Fetch

--- API/models/Receipts.js
+++ API/models/Receipts.js
@@ -4,13 +4,19 @@ var receipt = {
     getAllReceipt: (callback) =>{
         return db.query("SELECT * FROM RECEIPT", callback); 
     },
+    getAllCompleteReceipt: (callback) => {
+        return db.query("SELECT * FROM receipt WHERE TYPE=1" , callback);
+    },
+    getAllInCompleteReceipt: (callback) => {
+        return db.query("SELECT * FROM receipt WHERE TYPE=0" , callback);
+    },
     addAReceipt: (newReceipt, callback) =>{
         return db.query("INSERT INTO RECEIPT(RECEIPT_ID, DATECREATE, TOTALPRICE, STAFFID, CUSTOMER_ID, TBID) VALUES(?, ?, ?, ?, ?, ?",
             [newReceipt.RECRIPT_ID, newReceipt.DATECREATE, newReceipt.TOTALPRICE, newReceipt.STAFFID, newReceipt.CUSTOMER_ID, newReceipt.TBID], callback);
     }, 
     deleteAReceipt: (deletedReceipt, callback) => {
         return db.query("DELETE FROM RECEIPT WHERE RECEIPT_ID=?", [deletedReceipt], callback);
-    }, 
+    },
     getReceiptById: (RECEIPT_ID, callback) => {
         return db.query("SELECT * FROM receipt WHERE RECEIPT_ID = ?" , [RECEIPT_ID],  callback);
     }
